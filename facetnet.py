ea56f327d5170fcc7558fe4ed9d91cc750d937f9
Cristian Zanoci <czanoci@gmail.com>
2016-12-10 17:55:30 -0800

    ready to run version of facet net

--- facetnet.py
+++ facetnet.py
@@ -1,8 +1,10 @@
 from db_utils import *
 from graph_utils import *
 from facetnet_utils import *
+from metrics import *
 import snap
 import numpy as np 
+import timeit
 
 db_filename = 'D:/artist_full.db'
 
@@ -10,30 +12,128 @@ conn = open_db_conn(db_filename)
 
 artist_list = get_artists(conn)
 
-artist_id_to_int = {}
-nid_to_aid_map = {}
+artist_id_to_int, nid_to_aid_map = get_id_mapping(conn)
 
-for i in xrange(len(artist_list)):
-    artist_id_to_int[artist_list[i][0]] = i + 1 
-    nid_to_aid_map[i + 1] = artist_list[i][0]
-
-start_year = 1930
+start_year = 1950
 end_year = 2010
 alpha = 0.5
+num_iter = 20
 
 year_range = range(start_year - 1, end_year + 1)
-year_prev = False
-cluster_size_range = range(6, 16)
+
+years = []
+my_modularity_list = []
+term_metric_list = []
+temp_smoothness_list = []
+num_clusters_list = []
+
+clusters_prev = None
+window_half_size = 2
+min_num_clusters = 5
+max_num_clusters = 15
+num_clusters_prev = min_num_clusters + window_half_size
+#cluster_size_range = range(min_num_clusters, max_num_clusters+1)
 
 for year in year_range:
     print 'Building graph for', year
+    years.append(year)
     graph = build_graph(conn, year, artist_id_to_int)
 
-    W, X, L, all_vertex_ids, nid_to_index_map = initialize(graph, num_clusters)
-    num_iter = 0
-    if year_prev:
-    	X_prev = add_and_remove_nodes(X_prev, all_vertex_ids_prev, nid_to_index_map_prev, all_vertex_ids)
-    	X_next, L_next = update(W, X, L, X_prev, L_prev, alpha)
+    if graph.GetNodes() == 0:
+    	print 'Hit the end'
+    	break
+
+    # map num_clusters to modularity
+    num_clusters_to_modularity = {}
+    cluster_size_range = range(min(max(num_clusters_prev - window_half_size, min_num_clusters), max_num_clusters-2*window_half_size) , max(min(num_clusters_prev + window_half_size + 1, max_num_clusters + 1), min_num_clusters + 2*window_half_size+1))
+
+    X_max = None
+    L_max = None
+    all_vertex_ids_max = None
+    nid_to_index_map_max = None
+    modularity_max = float("-inf")
+    clusters_max = None
+    num_clusters_max = None
+
+    W, all_vertex_ids, nid_to_index_map = initialize(graph)
+    if clusters_prev is not None:
+    	Z_prev = add_and_remove_nodes(Z_prev, all_vertex_ids_prev, nid_to_index_map_prev, all_vertex_ids)
+
+    for num_clusters in cluster_size_range:
+		print num_clusters
+		X, L = initialize_XL(graph.GetNodes(), num_clusters)
+		
+		for i in xrange(num_iter):
+			#print "Update number " + str(i+1)
+			if clusters_prev is not None:
+				X, L = update(W, X, L, Z_prev, alpha)
+			else:
+				Z = np.zeros(W.shape)
+				X, L = update(W, X, L, Z, 1.0)
 
 
-    year_prev = True
+			'''
+			if year_prev:
+				#X_prev = add_and_remove_nodes(X_prev, all_vertex_ids_prev, nid_to_index_map_prev, all_vertex_ids, num_clusters)
+				#if num_iter == 1:
+				#Z_prev = add_and_remove_nodes(Z_prev, all_vertex_ids_prev, nid_to_index_map_prev, all_vertex_ids)
+				X_next, L_next = update(W, X, L, Z_prev, alpha)
+				if (num_iter % 10 == 0):
+					cost_next = get_cost(W, X_next, L_next, Z_prev, alpha)
+					cost = get_cost(W, X, L, Z_prev, alpha)
+			else:
+				Z = np.zeros(W.shape)
+				X_next, L_next = update(W, X, L, Z, 1.0)
+				Z_next = np.dot(X_next, np.dot(L_next, np.transpose(X_next)))
+				if (num_iter % 10 == 0):
+					cost_next = get_cost(W, X_next, L_next, Z_next, 1.0)
+					cost = get_cost(W, X, L, Z, 1.0)
+
+			X = X_next
+			L = L_next
+
+			if (num_iter % 10 == 0): #abs(cost - cost_next) < 0.01*cost:
+				if abs(cost - cost_next) < 0.02*cost:
+					break
+			'''
+		Q = soft_modularity(W, X, L)
+		num_clusters_to_modularity[num_clusters] = Q
+		if Q > modularity_max:
+				X_max = X
+				L_max = L
+				all_vertex_ids_max = all_vertex_ids
+				nid_to_index_map_max = nid_to_index_map
+				modularity_max = Q
+				clusters_max = compute_clusters(X, L, all_vertex_ids)
+				num_clusters_max = num_clusters
+
+    Z_prev = np.dot(X_max, np.dot(L_max, np.transpose(X_max)))
+    all_vertex_ids_prev = all_vertex_ids_max
+    nid_to_index_map_prev = nid_to_index_map_max
+
+    print "The maximum modularity is achieved with " + str(num_clusters_max) + " clusters"
+    print num_clusters_to_modularity
+
+    num_clusters_list.append(num_clusters_max)
+
+    print '  computing modularity'
+    my_modularity_list.append(get_modularity_metric(graph, clusters_max))
+
+    print '  computing term similarity'
+    term_metric_list.append(get_human_term_metric(clusters_max, nid_to_aid_map, conn))
+
+    if clusters_prev:
+		print '  computing temporal smoothness'
+		temp_smoothness_list.append(get_temporal_smoothness_metric(clusters_prev, clusters_max))
+    num_clusters_prev = num_clusters_max
+    clusters_prev = clusters_max
+
+    folder = 'output/facetnet/' + str(start_year) + '-' + str(end_year) + '/' + 'alpha=' + str(alpha) + '/'
+
+    np.savetxt(folder + 'years.txt', years, delimiter=',')
+    np.savetxt(folder + 'my_modularity_list.txt', my_modularity_list, delimiter=',')
+    np.savetxt(folder + 'term_metric_list.txt', term_metric_list, delimiter=',')
+    np.savetxt(folder + 'temp_smoothness_list.txt', temp_smoothness_list, delimiter=',')
+    np.savetxt(folder + 'num_clusters.txt', num_clusters_list, delimiter=',')
+
+close_db_conn(conn)
--- facetnet_utils.py
+++ facetnet_utils.py
@@ -3,6 +3,7 @@ from graph_utils import *
 import snap
 import numpy as np 
 from sklearn.preprocessing import normalize
+import timeit
 
 def KL_divergence(A, B):
 	(m, n) = A.shape
@@ -13,32 +14,34 @@ def KL_divergence(A, B):
 				div += A[i, j]*(np.log(float(A[i, j]) / B[i, j]) - 1) + B[i, j]
 	return div
 # L = Lambda matrix
-def cost(W, X, L, X_prev, L_prev, alpha):
+def get_cost(W, X, L, Z, alpha):
 	Y = np.dot(X, L)
-	Y_prev = np.dot(X_prev, L_prev)
+	#Y_prev = np.dot(X_prev, L_prev)
 	XLXT = np.dot(Y, np.transpose(X))
-	XLXT_prev = np.dot(Y_prev, np.transpose(X_prev))
+	#XLXT_prev = np.dot(Y_prev, np.transpose(X_prev))
 	CS = KL_divergence(W, XLXT)
-	CT = KL_divergence(XLXT_prev, XLXT)
+	CT = KL_divergence(Z, XLXT)
 	return alpha*CS + (1-alpha)*CT
 
-def D_inverse(X, L):
-	(n, m) = X.shape
+def D_inverse(Y):
+	(n, m) = Y.shape
 	D_inv = np.zeros((n, n))
-	Y = np.dot(X, L)
+	#Y = np.dot(X, L)
 	for i in xrange(n):
 		D_inv[i, i] = 1.0 / np.sum(Y[i, :])
+	return D_inv
 
 def soft_modularity(W, X, L):
 	(n, m) = X.shape
-	D_inv = D_inverse(X, L)
-	M = np.dot(D_inv, np.dot(X, L))
+	Y = np.dot(X, L)
+	D_inv = D_inverse(Y)
+	M = np.dot(D_inv, Y)
 	# assume W = W^T
 	N = np.dot(np.ones((1, n)), np.dot(W, M))
 	Q_s = np.trace(np.dot(np.transpose(M), np.dot(W, M))) - np.dot(N, np.transpose(N))
-	return Q_s
+	return Q_s[0][0]
 
-def initialize(graph, num_clusters):
+def initialize(graph):
 	n = graph.GetNodes()
 	num_edges = graph.GetEdges()
 
@@ -59,40 +62,55 @@ def initialize(graph, num_clusters):
 			if graph.IsEdge(node_i, node_j):
 				W[i, j] = 1.0 / (2 * num_edges)
 
-	X = (1.0 / n) * np.zeros((n, n))
+	return W, all_vertex_ids, nid_to_index_map
+
+def initialize_XL(n, num_clusters):
+	#X = (1.0 / n) * np.ones((n, num_clusters))
+	#np.random.seed(42)
+	X = np.random.rand(n, num_clusters)
+	X = normalize(X, axis = 0, norm = 'l1')
 
 	L = np.diag([1.0/num_clusters for _ in xrange(num_clusters)])
+	return X, L
 
-	return W, X, L, all_vertex_ids, nid_to_index_map
-
-def add_and_remove_nodes(X_prev, all_vertex_ids_prev, nid_to_index_map_prev, all_vertex_ids):
+def add_and_remove_nodes(Z_prev, all_vertex_ids_prev, nid_to_index_map_prev, all_vertex_ids):
 	n = len(all_vertex_ids)
-	X_prev_adjusted = np.zeros((n, n))
+	Z_prev_adjusted = np.zeros((n, n))
 	for i in xrange(n):
-		node = all_vertex_ids[i]
-		if node in all_vertex_ids_prev:
-			X_prev_adjusted[i, :] = X_prev[nid_to_index_map_prev[node], :]
-	return X_prev_adjusted
+		for j in xrange(n):
+			node_i = all_vertex_ids[i]
+			node_j = all_vertex_ids[j]
+			if (node_i in all_vertex_ids_prev) and (node_j in all_vertex_ids_prev):
+				Z_prev_adjusted[i, j] = Z_prev[nid_to_index_map_prev[node_i], nid_to_index_map_prev[node_j]]
+	Z_prev_adjusted = 1.0/np.sum(np.sum(Z_prev_adjusted)) * Z_prev_adjusted
+	return Z_prev_adjusted
 
 
-def update(W, X, L, X_prev, L_prev, alpha):
+def update(W, X, L, Z, alpha):
 	XLXT = np.dot(X, np.dot(L, np.transpose(X)))
-	Z = np.dot(X_prev, np.dot(L_prev, np.transpose(X_prev)))
+	#Z = np.dot(X_prev, np.dot(L_prev, np.transpose(X_prev)))
 
 	X_next = np.zeros(X.shape)
 	L_next = np.zeros(L.shape)
 	(n, m) = X.shape
-
 	for k in xrange(m):
+		sum_i = np.zeros(n)
 		for i in xrange(n):
+			with np.errstate(divide='ignore', invalid='ignore'):
+				v = np.divide(alpha*W[i, :] + (1 - alpha)*Z[i, :], XLXT[i, :])
+				v[XLXT[i, :] == 0] = 0
+			sum_j = np.dot(v, X[:, k])
+			'''
 			sum_j = 0
 			for j in xrange(n):
-				if XLXT[i, j] != 0:
-					sum_j += (alpha*W[i, j] + (1 - alpha)*Z[i, j]) * X[j, k] / XLXT[i, j]
-			X_next[i, k] = X[i, k] * L[k, k] * sum_j
-			L_next[k, k] += X[i, k] * sum_j
+				elem = XLXT[i, j]
+				if elem != 0:
+					sum_j += (alpha*W[i, j] + (1 - alpha)*Z[i, j]) * X[j, k] / elem
+			'''
+			sum_i[i] = sum_j
+			X_next[i, k] = X[i, k] * sum_j * L[k, k]
+		L_next[k, k] = np.dot(sum_i, X[:, k])
 		L_next[k, k] *= L[k, k]
-
 	X_next = normalize(X_next, axis = 0, norm = 'l1')
 	L_next = normalize(np.diag(L_next).reshape(1, -1), norm = 'l1')
 	L_next = np.diag(L_next[0])
@@ -100,23 +118,17 @@ def update(W, X, L, X_prev, L_prev, alpha):
 
 def compute_clusters(X, L, all_vertex_ids):
 	(n, m) = X.shape
-	D_inv = D_inverse(X, L)
 	Y = np.dot(X, L)
+	D_inv = D_inverse(Y)
 	member = np.dot(D_inv, Y)
 	communities = [snap.TIntV() for _ in xrange(m)]
 	for node in xrange(n):
-		comm = np.argmax(member[i, :])
+		comm = np.argmax(member[node, :])
 		communities[comm].Add(all_vertex_ids[node])
 	# potentially remove empty communities
 	clusters = snap.TCnComV()
 	for cluster in communities:
 		if not cluster.Empty():
 			clusters.Add(snap.TCnCom(cluster))
-	return communities
+	return clusters
 
-D = np.array([[1.0, 2], [2.0, 3]])
-print normalize(D, axis = 0, norm = 'l1')
-D = normalize(np.diag(D).reshape(1, -1), norm = 'l1')
-print np.diag(D[0])
-a = [1, 2, 4, -1]
-print np.sort(a)
